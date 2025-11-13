9d9da506f05e66f19e7f9713b25634cbd5316a43
NeroPcStation <neroxbox28@gmail.com>
2025-11-13 18:54:42 +0100

    Refactor MOCK environment variable check for product retrieval

--- src/Service/Biedronka/BiedronkaProductService.php
+++ src/Service/Biedronka/BiedronkaProductService.php
@@ -24,7 +24,7 @@ class BiedronkaProductService
      */
     public function getProduct(string $barcode): string
     {
-        if ($_ENV['MOCK']) {
+        if (filter_var($_ENV['MOCK'] ?? false, FILTER_VALIDATE_BOOLEAN)) {
             $mock = '{"theme_name":"AlwaysLowPrices","name":"NAPÓJ GAZOWANY COCA-COLA 0,5l","unit":"but","unit_price":["9,98 zł/l"],"price":"4.99","promotion_valid_period":null,"details":"","limit_message":null,"only_with_biedronka_card":false,"regular_price":"4.99","is_promotion":false,"is_minimal_basket_value_bonus_buy":false,"discount":null,"price_tag_info":null,"price_tag_note":null,"omnibus_price":"0.00","discount_to_omnibus_price":null,"omnibus_type":null,"description":"","type_of_wine":null,"sweetness_of_wine":null,"country_of_origin":null,"product_additional_country_of_origin_info":false,"fruits_or_vegetables_class":null,"image_url":"https://assets.prod.biedronka.cloud/products/resized/472_189.webp","thumb_url":"https://assets.prod.biedronka.cloud/products/resized/472_189.webp","multipack":null,"is_multipack":false,"omnibus_price_line":null,"regular_price_line":"Cena za but: 4.99 zł (9,98 zł/l)","non_promotional":false,"badges":[]}';
 
             return $mock;
